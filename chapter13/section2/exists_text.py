c9b48fc3947c9070dc557678ec7fe968a04a8515
kamoshin <kamoshin0107@icloud.com>
2019-07-13 22:15:48 +0900

    フォルダがなければ作成し、ファイルがあれば上書きするかどうか確認する

--- chapter13/section2/exists_text.py
+++ chapter13/section2/exists_text.py
@@ -6,16 +6,16 @@ folder = "./chapter13/data/"
 file = folder + "sample.txt"
 
 def filewrite():
-    if not os.path.exists(folder):
-        os.makedirs(folder)
+    if not os.path.exists(folder):      #保存フォルダがあるか
+        os.makedirs(folder)             #フォルダを作る
     with open(file, "w", encoding="utf_8") as fileobj:
         num = randint(0, 100)
         fileobj.write(f"{num}が出ました")
         print("ファイルを保存しました")
 
-if os.path.exists(file):
+if os.path.exists(file):        #既存ファイルがあるか
     while True:
-        answer = input("上書きしてもよいですか？(y/n)")
+        answer = input("上書きしてもよいですか？(y/n)") #確認
         if answer == "y":
             filewrite()
             break
