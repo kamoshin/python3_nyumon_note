769e944f9bbff09facc1d440c817c15aa67f678a
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-07-14 00:59:57 +0900

    x軸price、y軸cpuntのグラフを作る

---
+++ chapter14/section1/pyplot_xy.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt     #matplotlib.pyplotモジュールを読み込む
+price = [200, 300, 400, 500, 600]
+count = [31, 29, 25, 28, 26]
+plt.plot(price, count)  #グラフを描く
+plt.show()      #表示する
