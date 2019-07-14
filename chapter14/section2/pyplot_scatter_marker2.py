0a628bcfe12c451c0ca416aa64e38944450ddf4b
kamoshin <kamoshin0107@icloud.com>
2019-07-14 17:31:37 +0900

    マーカーのサイズ、透明度、縁の幅、縁線の色などを設定した散布図

--- chapter14/section2/pyplot_scatter_marker2.py
+++ chapter14/section2/pyplot_scatter_marker2.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
-X, Y = np.random.rand(100), np.random.rand(100)
+X, Y = np.random.rand(100), np.random.rand(100)     #ランダムな配列を作る
+#sはマーカーのサイズ、alphaは透明度、linewidthsは線幅、edgecolorsで縁色の指定
 plt.scatter(X, Y, marker = "o", s = 500, color = "cyan", alpha = 0.5, linewidths = 2, edgecolors = "b")
 plt.show()
