beb048d5166afc454518c4396bf5c59099409b64
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-01 15:19:43 +0900

    配列をリストに戻す

--- chapter15/section1/np1-6.py
+++ chapter15/section1/np1-6.py
@@ -3,5 +3,5 @@ a = np.array([1,2,3,4,5])
 b = np.array([[1,2],[3,4],[5,6]])
 print(a)
 print(b)
-print(a.tolist())
-print(b.tolist())
+print(a.tolist())   #配列をリストに変換する
+print(b.tolist())   #多次元配列は多次元リストになる
