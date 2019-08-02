54f9c8814b4b2c63d6a500bd5611afc35d998cd1
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:15:58 +0900

    配列のスライス

--- chapter15/section2/np2-2.py
+++ chapter15/section2/np2-2.py
@@ -1,8 +1,8 @@
 import numpy as np
 data = np.array([0,10,20,30,40,50,60,70,80,90])
-print(data[:])
-print(data[:4])
-print(data[4:])
-print(data[3:7])
-print(data[::2])
-print(data[::-1])
+print(data[:])      #全ての要素(複製)
+print(data[:4])     #最初からインデックス番号３まで
+print(data[4:])     #インデックス番号４から最後まで
+print(data[3:7])    #インデックス番号３からインデックス番号７まで
+print(data[::2])    #先頭から１個とび
+print(data[::-1])   #末尾から１個ずつ取り出す
