fb0f7d5440daabce6bb1ac222ac0a8e507d2f6ee
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:16:24 +0900

    すべての要素を順に取り出す

--- chapter15/section2/np2-3.py
+++ chapter15/section2/np2-3.py
@@ -2,12 +2,12 @@ import numpy as np
 data = [10,20,30,40,50,60,70,80,90]
 a = np.array(data).reshape(3,3)
 print(a)
-print(a[:2,])
-print(a[:,1:])
-print(a[1:,1:])
+print(a[:2,])       #０～１行目、すべての列
+print(a[:,1:])      #全ての行、１列目以降
+print(a[1:,1:])     #１行目以降、１列目以降
 
 data = [2.1, 3.5, 2.5, 4.3, 5.1, 1.6]
 a = np.array(data).reshape(3,2)
 print(a)
-a2 = a[:2,].astype(int)
-print(a2)
+a2 = a[:2,].astype(int)     #最初の２行を取り出す際に整数に変換する
+print(a2)       #値が整数に変換されている
