80cce8794abf75fa9a941d3357e470a6448067a5
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:17:02 +0900

    条件式で要素を抽出する

--- chapter15/section2/np2-4.py
+++ chapter15/section2/np2-4.py
@@ -1,4 +1,4 @@
 import numpy as np
 data = np.array([10,20,30,40,50,60]).reshape(2,3)
-for i, item in np.ndenumerate(data):
-    print(i, item)
+for i, item in np.ndenumerate(data):    #多次元配列から位置と要素を取り出す
+    print(i, item)      #iには(行、列)のタプルが入る
