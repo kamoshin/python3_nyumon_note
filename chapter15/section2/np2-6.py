3d0ae580e440918bb77d3f20db5fe4e390cd2973
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:18:45 +0900

    条件に合う要素を変更する

--- chapter15/section2/np2-6.py
+++ chapter15/section2/np2-6.py
@@ -2,7 +2,7 @@ import numpy as np
 a = np.array([3,1,6,3,7,2,9,6,5,2,1,7,8,3,1,3])
 a = a.reshape(4,4)
 print(a)
-a[a%2 == 0] = 0
+a[a%2 == 0] = 0     #偶数を０に置き換える
 print(a)
-a[a%2 == 1] = 1
+a[a%2 == 1] = 1     #奇数を１に置き換える
 print(a)
