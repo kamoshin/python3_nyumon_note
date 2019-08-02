103bd5c758abd3952efb74a15e96926440e7ec07
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:17:45 +0900

    条件に合う要素を変更する

--- chapter15/section2/np2-5.py
+++ chapter15/section2/np2-5.py
@@ -1,11 +1,11 @@
 import numpy as np
 a = np.array([1,2,3,4,5,6,7,8,9])
-print(a[a>=5])
-print(a[a%2 == 0])
-print(a[a%2 == 1])
+print(a[a>=5])      #５以上の値を抽出
+print(a[a%2 == 0])  #偶数
+print(a[a%2 == 1])  #奇数
 
 b = a.reshape(3,3)
-print(b[b>5])
-print(a[(a>=5) & (a%2 == 1)])
-print(a[(a%2 == 0) | (a%3 == 0)])
-print(a[~(a%3 == 0)])
+print(b[b>5])   #多次元配列から５より大きい値
+print(a[(a>=5) & (a%2 == 1)])       #論理積(５以上かつ奇数)
+print(a[(a%2 == 0) | (a%3 == 0)])   #論理和(２または３の倍数)
+print(a[~(a%3 == 0)])   #論理否定(３の倍数ではない)
