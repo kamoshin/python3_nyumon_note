5fd686fe495b7459d5e2fdced4146478545bd109
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-02 23:36:26 +0900

    配列のスライス(応用)

--- chapter15/section2/np2-3.py
+++ chapter15/section2/np2-3.py
@@ -1,4 +1,5 @@
 import numpy as np
+#多次元配列のスライス
 data = [10,20,30,40,50,60,70,80,90]
 a = np.array(data).reshape(3,3)
 print(a)
@@ -6,6 +7,7 @@ print(a[:2,])       #０～１行目、すべての列
 print(a[:,1:])      #全ての行、１列目以降
 print(a[1:,1:])     #１行目以降、１列目以降
 
+#スライスと同時に型変換する
 data = [2.1, 3.5, 2.5, 4.3, 5.1, 1.6]
 a = np.array(data).reshape(3,2)
 print(a)
