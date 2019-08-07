41a0ac92994a5a8648c570a928dcfa0df0b8c3bd
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-07 11:17:07 +0900

    ０または１で埋まった配列を作る

---
+++ chapter15/section4/np.4-5.py
@@ -0,0 +1,12 @@
+import numpy as np
+#０が９個埋まった配列を作る
+ZERO = np.zeros(9, dtype=int)
+print(ZERO)
+
+#１が９個埋まった配列を作る
+ONE = np.ones(9, dtype=int)
+print(ONE)
+
+#全てが整数１の２行３列の配列を作る
+ONE = np.ones((2,3), dtype=int)
+print(ONE)
