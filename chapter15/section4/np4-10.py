798076ea7e608ca81dbead24be0b2aecc0e3e73a
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-07 11:46:51 +0900

    csvファイルから読み込む

--- chapter15/section4/np4-10.py
+++ chapter15/section4/np4-10.py
@@ -1,10 +1,11 @@
 import numpy as np
+#カンマ区切りを読み込み、1行目を読み込まない
 data = np.loadtxt("chapter15/section4/data.csv", delimiter=",", skiprows=1)
 print(data)
 
-import pandas as pd
+import pandas as pd     #pandasモジュール
 df = pd.read_csv("chapter15/section4/data.csv")
-header = df.columns.values
-data = df.values
+header = df.columns.values  #ヘッダ行
+data = df.values    #データ部分
 print(header)
 print(data)
