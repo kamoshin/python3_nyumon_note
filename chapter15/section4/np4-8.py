6a011e60ccc53c78e40ac892d2ae75193a9f7833
kamoshin <b1701788@planet.kanazawa-it.ac.jp>
2019-08-07 11:46:03 +0900

    同じ乱数を再現する

--- chapter15/section4/np4-8.py
+++ chapter15/section4/np4-8.py
@@ -1,5 +1,5 @@
 import numpy as np
-np.random.seed(10)
-print(np.random.randn(3))
-np.random.seed(10)
-print(np.random.randn(3))
+np.random.seed(10)      #ランダムシードを設定する
+print(np.random.randn(3))   #乱数を発生
+np.random.seed(10)      #同じランダムシードを設定する
+print(np.random.randn(3))   #同じ乱数が作られる
